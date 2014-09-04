8da4a1d5d0fe6f3f9e2ce555082d0cf9c2417dbb
Wil Alvarez <wil.alvarez@groupon.com>
2014-09-04 11:37:37 -0400

    Removed manual python script in favor of gulp

--- build.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for r, d, files in os.walk('.'):
-    for f in files:
-        p = os.path.splitext(f)
-        if p[1] == '.js':
-            cmd = "minify %s.js > %s.min.js" % (p[0], p[0])
-            print "Minifing %s" % f
-            os.system(cmd)
