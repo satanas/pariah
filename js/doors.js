c387f4a5417a2f6c84eb4c3da735d49a919b9418
Wil Alvarez <wil.alvarez@groupon.com>
2014-09-06 17:05:54 -0400

    Cleaned out some code

--- js/doors.js
+++ js/doors.js
@@ -1,40 +1,32 @@
-$.Entrance = function(x, y, hf) {
+$.Entrance = function(x, y) {
   this.x = x;
   this.y = y;
   this.w = 32;
   this.h = 32;
-  this.half = hf;
   this.t = $.util.byId('tileset');
-  this.r = Math.sqrt(Math.pow(this.w/2, 2) + Math.pow(this.h/2, 2));
-  var inView = 0;
 
   this.bounds = {
-    b: (this.half === 0) ? this.y + this.h : this.y + (this.h / 2),
+    b: this.y + (this.h / 2),
     t: this.y,
     l: this.x,
     r: this.x + this.w
   };
 
   this.render = function(tx, ty) {
-    //var tx = x - $.ofx;
-    //var ty = y - $.ofy;
-
-    //if ((tx >= -w && tx < $.vw) && (ty >= -y && ty < $.vh)) {
-      $.ctxfg.save();
-      $.ctxfg.fillStyle = 'rgb(131, 0, 0)';
-      $.ctxfg.fillRect(tx, ty, 32, 32);
-      $.ctxfg.restore();
-    //}
+    $.ctxfg.save();
+    $.ctxfg.scale(2.0, 2.0);
+    $.ctxfg.drawImage(this.t, 0, 0, 16, 16, tx/2, ty/2, 16, 16);
+    $.ctxfg.drawImage(this.t, 0, 48, 16, 16, tx/2, ty/2, 16, 16);
+    $.ctxfg.restore();
   };
 };
 
-$.Exit = function(x, y, hf) {
+$.Exit = function(x, y) {
   this.x = x;
   this.y = y;
   this.w = 32;
   this.h = 32;
   this.t = $.util.byId('tileset');
-  var inView = 0;
 
   this.bounds = {
     b: this.y + (this.h / 4),
@@ -44,14 +36,9 @@ $.Exit = function(x, y, hf) {
   };
 
   this.render = function(tx, ty) {
-    //var tx = x - $.ofx;
-    //var ty = y - $.ofy;
-
-    //if ((tx >= -w && tx < $.vw) && (ty >= -y && ty < $.vh)) {
-      $.ctxfg.save();
-      $.ctxfg.fillStyle = 'rgb(0, 131, 0)';
-      $.ctxfg.fillRect(tx, ty, 32, 32);
-      $.ctxfg.restore();
-    //}
+    $.ctxfg.save();
+    $.ctxfg.scale(2.0, 2.0);
+    $.ctxfg.drawImage(this.t, 16, 48, 16, 16, tx/2, ty/2, 16, 16);
+    $.ctxfg.restore();
   };
 };
--- js/wall.js
+++ js/wall.js
@@ -5,8 +5,6 @@ $.Wall = function(x, y, hf) {
   this.h = 32;
   this.half = hf;
   this.t = $.util.byId('tileset');
-  this.r = Math.sqrt(Math.pow(this.w/2, 2) + Math.pow(this.h/2, 2));
-  var inView = 0;
 
   this.bounds = {
     b: (this.half === 0) ? this.y + this.h : this.y + (this.h / 2),
@@ -16,20 +14,13 @@ $.Wall = function(x, y, hf) {
   };
 
   this.render = function(tx, ty) {
-    //var tx = x - $.ofx;
-    //var ty = y - $.ofy;
-
-    //if ((tx >= -w && tx < $.vw) && (ty >= -y && ty < $.vh)) {
-      $.ctxfg.save();
-      //$.ctxfg.fillStyle = 'rgb(131, 73, 11)';
-      //$.ctxfg.fillRect(tx, ty, 32, 32);
-      $.ctxfg.scale(2.0, 2.0);
-      if (this.half) {
-        $.ctxfg.drawImage(this.t, 0, 17, 16, 16, tx/2, ty/2, 16, 16);
-      } else {
-        $.ctxfg.drawImage(this.t, 0, 0, 16, 16, tx/2, ty/2, 16, 16);
-      }
-      $.ctxfg.restore();
-    //}
+    $.ctxfg.save();
+    $.ctxfg.scale(2.0, 2.0);
+    if (this.half) {
+      $.ctxfg.drawImage(this.t, 0, 0, 16, 16, tx/2, ty/2, 16, 16);
+    } else {
+      $.ctxfg.drawImage(this.t, 0, 0, 16, 16, tx/2, ty/2, 16, 16);
+    }
+    $.ctxfg.restore();
   };
 };
