05acd1508bcca94aaa9dfbc67415baf41d199566
Wil Alvarez <wil.alvarez@groupon.com>
2014-09-10 20:31:41 -0300

    Optimized colors on textpops

--- js/textpop.js
+++ js/textpop.js
@@ -1,4 +1,4 @@
-$.TextPop = function(text, x, y, color) {
+$.TextPop = function(text, x, y, c) {
   var _ = this;
   _.x = x;
   _.y = y;
@@ -8,19 +8,7 @@ $.TextPop = function(text, x, y, color) {
   _.ctime = $.n();
   _.text = text;
   _.bounds = {r:0, b:0};
-  _.colors = {
-    white: 'rgb(255,255,255)',
-    yellow: 'rgb(255,255,0)',
-    red: 'rgb(255,0,0)',
-    green: $.HCOLOR,
-    blue: $.MCOLOR
-  };
-  if (color === undefined)
-    _.c = _.colors.white;
-  else if (_.colors.hasOwnProperty(color))
-    _.c = _.colors[color];
-  else
-    _.c = _.colors.white;
+  _.c = c;
 
   _.update = function(i) {
     if (_.y > _.oy)
