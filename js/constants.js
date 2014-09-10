12900288c75b36957bfff74cfee056a79acd5acf
Wil Alvarez <wil.alvarez@groupon.com>
2014-09-10 00:27:43 -0300

    Optimized colors

--- js/constants.js
+++ js/constants.js
@@ -28,11 +28,12 @@ $.C = {
   f: 'hsl(326,10%,25%)', // Floor color
   b: 'rgb(0,0,0)',
   w: 'rgb(255,255,255)',
-  o: 'hsla(13,100%,51%,1)', // Orange fire
-  u: 'hsla(196,90%,76%,1)', // Blue water
-  e: 'hsla(28,65%,42%,1)', // Brown earth
-  s: 'hsla(78,100%,92%,1)', // Blue sky
-  g: 'rgb(154,154,154)', // Gray
-  r: 'rgb(200,0,0)' // Eed
+  o: 'hsl(13,100%,51%)', // Orange fire
+  u: 'hsl(196,90%,76%)', // Blue water
+  e: 'hsl(28,66%,26%)', // Brown earth
+  s: 'hsl(78,100%,92%)', // Blue sky
+  gry: 'rgb(154,154,154)', // Gray
+  r: 'rgb(200,0,0)', // Eed
+  gld: 'rgb(219,190,117)',
 
 };
