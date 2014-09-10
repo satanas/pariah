ad4a748f37c34e96c41671ca47ce53f3902e36bf
Wil Alvarez <wil.alvarez@groupon.com>
2014-09-10 00:26:13 -0300

    Finished HUD

--- js/hud.js
+++ js/hud.js
@@ -8,18 +8,17 @@ $.Hud = function() {
     $.x.fr(55, 5, 80, 10);
     $.x.fr(55, 20, 80, 10);
     $.x.fillStyle = $.C.w;
-    $.x.fillText('HEALTH', 10, 15);
-    $.x.fillText('MANA', 10, 30);
-    $.x.fillText('LEVEL ' + $.lv, 560, 15);
-    $.x.fillText('SCORE 9999', 560, 30);
+    $.x.ft('HEALTH', 10, 15);
+    $.x.ft('MANA', 10, 30);
+    $.x.ft('LEVEL ' + $.lv, 560, 15);
+    $.x.ft('SCORE 9999', 560, 30);
 
     var co = [$.C.o, $.C.e, $.C.u, $.C.s],
-        c = 0;
-    for (c in co) {
-      if ($.hero.pows.indexOf(c) < 0) continue;
-      $.x.fillStyle = co[c];
+        i;
+    for (i in $.epow) {
+      $.x.fillStyle = co[$.epow[i] - 1];
       $.x.beginPath();
-      $.x.arc(150 + (15 * c), 10, 4, 0, (2 * Math.PI), 0);
+      $.x.arc(150 + (15 * $.epow[i]), 10, 4, 0, (2 * Math.PI), 0);
       $.x.fill();
     }
 
