9ad0b7e9cb4c8144b8c35aad6e611202a5633fd8
Wil Alvarez <wil.alvarez@groupon.com>
2014-09-12 20:40:25 -0300

    Fixed order of js in gulp task. Added task to build .zip

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,13 @@ var minifyHTML = require('gulp-minify-html');
 var clean = require('gulp-clean');
 var glob = require('glob');
 var exec = require('child_process').exec;
+var zip = require('gulp-zip');
 
 gulp.task('default', function() {
-  gulp.src('js/*.js')
+  //gulp.src('js/*.js')
+  gulp.src(['js/constants.js', 'js/rect.js', 'js/util.js', 'js/input.js', 'js/camera.js', 'js/collisions.js', 'js/powers.js',
+    'js/items.js', 'js/switches.js', 'js/hero.js', 'js/enemies.js', 'js/wall.js', 'js/doors.js', 'js/fow.js', 'js/hud.js',
+    'js/scene.js', 'js/deco.js', 'js/level.js', 'js/textpop.js', 'js/ai.js', 'js/game.js' ])
   .pipe(concat('all.min.js'))
   .pipe(uglify())
   .pipe(gulp.dest('min'));
@@ -34,6 +38,12 @@ gulp.task('clean', function() {
 gulp.task('uglify', function() {
   gulp.src('js/*.js')
   .pipe(uglify())
+  .pipe(gulp.dest('min'));
+});
+
+gulp.task('build', function() {
+  gulp.src(['min/all.min.js', 'min/index.html', 'min/style.css', 'min/t.gif'])
+  .pipe(zip('pariah.zip'))
   .pipe(gulp.dest('min'));
 });
 
